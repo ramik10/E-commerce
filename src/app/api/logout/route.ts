af18a761f4d9d434f72677c30416e6cafe55aa23
RAMIK MUKHERJEE <ramik.14062000@gmail.com>
2023-09-15 02:34:26 +0530

    used next auth with db and prisma

--- src/app/api/logout/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-
-export function GET() {
-    const jsonResponse = NextResponse.json({ success: true, message: "Logging out" });
-    jsonResponse.cookies.delete("token")
-    return jsonResponse;
-}
