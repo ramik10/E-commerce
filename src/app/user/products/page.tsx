16564f8912a09871ae6a361fb8e10be6d9b1ec37
RAMIK MUKHERJEE <ramik.14062000@gmail.com>
2023-09-15 02:49:05 +0530

    used next auth with db and prisma1

--- src/app/user/products/page.tsx
+++ src/app/user/products/page.tsx
@@ -1,6 +1,6 @@
 
 
-export  function products() {
+export  function Products() {
     return (
         <div style={{display:"flex", justifyContent:"center", paddingTop:"3vh"}}>
         <img  style={{width:"35vw"}} src="/pngegg (1).png" />
