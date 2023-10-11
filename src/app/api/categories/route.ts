59236a8da111396447c21b23a485b14c12ee224d
RAMIK MUKHERJEE <ramik.14062000@gmail.com>
2023-10-11 22:52:12 +0530

    ui change

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,7 +1,7 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { Prisma } from "@/Db/prismaFile";
 
-export async function GET() {
+export async function GET(req: NextRequest) {
     const categories = await Prisma.category.findMany();
     return NextResponse.json(categories)
 }
